0dbf37fbf109246efd512bae97428416a9878ebc
naimatalar <istemi1016@gmail.com>
2022-09-16 14:24:06 +0300

    nbvnbv

--- components/LanguageContent.js
+++ components/LanguageContent.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { Avatar } from "react-native-paper";
 import {DevSettings} from 'react-native';
+import * as Updates from 'expo-updates';
 function LanguageContent() {
     const [flg, setFlg] = useState()
     const [showFlg, setShowGFlg] = useState(false)
@@ -33,10 +34,10 @@ console.log(lnconf)
         </TouchableOpacity>}
 
       { showFlg&& <View style={{ position: "absolute", top: "100%", backgroundColor: "white", padding: 15, right: 1, borderWidth: 5, borderColor: "#393185", borderStyle: "solid" }}>
-            <TouchableOpacity  onPress={()=>{AsyncStorage.setItem("language","tr-US");DevSettings.reload()}}>
+            <TouchableOpacity  onPress={()=>{AsyncStorage.setItem("language","tr-US");Updates.reloadAsync()}}>
                 <Image style={{ resizeMode: "contain", width: 50, height: 50 }} source={require("../assets/trk.png")}></Image>
             </TouchableOpacity>
-            <TouchableOpacity onPress={()=>{AsyncStorage.setItem("language","en-US");DevSettings.reload()}}>
+            <TouchableOpacity onPress={()=>{AsyncStorage.setItem("language","en-US");;Updates.reloadAsync()}}>
                 <Image style={{ resizeMode: "contain", width: 50, height: 50 }} source={require("../assets/ing.png")}></Image>
             </TouchableOpacity>
         </View>}
