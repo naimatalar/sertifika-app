f216c78b522a6a8e4f58ccf171496c4156ca42a1
naimatalar <istemi1016@gmail.com>
2022-09-21 22:15:22 +0300

    ty

---
+++ components/DocumentApplication.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Modal, Text, View } from 'react-native';
+
+function DocumentApplication({visible=false}) {
+    return (
+        
+            <Modal visible={visible}>
+             <Text>dssd</Text>
+            </Modal>
+        
+    );
+}
+
+export default DocumentApplication;
